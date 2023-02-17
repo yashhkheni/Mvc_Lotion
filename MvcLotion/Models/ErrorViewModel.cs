66a9f69bde31fe7a961cab86a957ff06f28e6336
Yash <Yash@MEET>
2023-02-17 00:26:20 -0500

    Add project files.

---
+++ MvcLotion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcLotion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
